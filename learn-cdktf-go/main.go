820370b1fa47f86fc333e202323e7fcc3874e60f
Aurelie Vache <scraly@gmail.com>
2024-02-27 15:59:30 +0000

    cdktf: working example with kube and nodepool

--- learn-cdktf-go/main.go
+++ learn-cdktf-go/main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"os"
+
+	"cdk.tf/go/stack/generated/hashicorp/local/file"
+	local "cdk.tf/go/stack/generated/hashicorp/local/provider"
+	"cdk.tf/go/stack/generated/ovh/ovh/cloudprojectkube"
+	"cdk.tf/go/stack/generated/ovh/ovh/cloudprojectkubenodepool"
+
 	ovh "cdk.tf/go/stack/generated/ovh/ovh/provider"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -10,8 +17,7 @@ import (
 func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 	stack := cdktf.NewTerraformStack(scope, &id)
 
-	// The code that defines your stack goes here
-
+	// Initialize the OVH provider
 	ovh.NewOvhProvider(stack, jsii.String("ovh"), &ovh.OvhProviderConfig{
 		Endpoint: jsii.String("ovh-eu"), //required
 		//ApplicationKey:    jsii.String(""),       //required OVH_APPLICATION_KEY
@@ -19,25 +25,48 @@ func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 		//ConsumerKey:       jsii.String(""),       //required OVH_CONSUMER_KEY
 	})
 
-	//CloudProjectKube
+	// Initialize the Local provider
+	local.NewLocalProvider(stack, jsii.String("local"), &local.LocalProviderConfig{})
 
-	//cloudproject.NewKube()
+	//Get serviceName from the env var OVH_CLOUD_PROJECT_SERVICE
+	serviceName := os.Getenv("OVH_CLOUD_PROJECT_SERVICE")
 
 	// Deploy a new Kubernetes cluster
-	//myKube, err := cloudproject.NewKube(ctx, "my_desired_cluster", &cloudproject.KubeArgs{
-	//	ServiceName: pulumi.String(serviceName),
-	//	Name:        pulumi.String("my_desired_cluster"),
-	//	Region:      pulumi.String("GRA5"),
+	//TODO: Handle serviceName with a variable
+	kube := cloudprojectkube.NewCloudProjectKube(stack, jsii.String("my_desired_cluster"), &cloudprojectkube.CloudProjectKubeConfig{
+		ServiceName: jsii.String(serviceName),
+		Name:        jsii.String("my_desired_cluster"),
+		Region:      jsii.String("GRA5"),
+	})
+
+	cdktf.NewTerraformOutput(stack, jsii.String("cluster_version"), &cdktf.TerraformOutputConfig{
+		Value: kube.Version(),
+	})
+
+	//cdktf.NewTerraformOutput(stack, jsii.String("kubeconfig_file"), &cdktf.TerraformOutputConfig{
+	//	Value:     kube.Kubeconfig(),
+	//	Sensitive: jsii.Bool(true),
 	//})
 
-	//instance := ec2.NewInstance(stack, jsii.String("cdktfgo"), &ec2.InstanceConfig{
-	//    Ami:          jsii.String("ami-04c921614424b07cd"),
-	//    InstanceType: jsii.String("t2.micro"),
-	//})
+	file.NewFile(stack, jsii.String("kubeconfig"), &file.FileConfig{
+		Filename: jsii.String("kubeconfig.yaml"),
+		Content:  kube.Kubeconfig(),
+	})
 
-	//cdktf.NewTerraformOutput(stack, jsii.String("public_ip"), &cdktf.TerraformOutputConfig{
-	//    Value: instance.PublicIp(),
-	//})
+	// Deploy a Node Pool
+	nodePool := cloudprojectkubenodepool.NewCloudProjectKubeNodepool(stack, jsii.String("my-pool"), &cloudprojectkubenodepool.CloudProjectKubeNodepoolConfig{
+		ServiceName:  kube.ServiceName(),
+		KubeId:       kube.Id(),
+		Name:         jsii.String("my-pool"),
+		DesiredNodes: jsii.Number(1),
+		MaxNodes:     jsii.Number(3),
+		MinNodes:     jsii.Number(1),
+		FlavorName:   jsii.String("b2-7"),
+	})
+
+	cdktf.NewTerraformOutput(stack, jsii.String("nodePoolID"), &cdktf.TerraformOutputConfig{
+		Value: nodePool.Id(),
+	})
 
 	return stack
 }
